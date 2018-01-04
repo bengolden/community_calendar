c413ba93a5d6f3a5169e315b7b3dca7198344838
Ben Golden <bengolden1@gmail.com>
2018-01-04 17:07:47 -0500

    adjust new event button, add site model with site name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171208173743) do
+ActiveRecord::Schema.define(version: 20180104220543) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,10 @@ ActiveRecord::Schema.define(version: 20171208173743) do
     t.string   "tickets_url"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+  end
+
+  create_table "sites", force: :cascade do |t|
+    t.string "name"
   end
 
 end
