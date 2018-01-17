71b55cf9b0e5f979dd4db6bd62da26f17664693d
Ben Golden <bengolden1@gmail.com>
2018-01-17 12:59:44 -0600

    create site in create sites migration

--- db/migrate/20180104220543_create_sites.rb
+++ db/migrate/20180104220543_create_sites.rb
@@ -3,5 +3,7 @@ class CreateSites < ActiveRecord::Migration[5.0]
     create_table :sites do |t|
       t.string :name
     end
+
+    Site.create(name: 'Chippewa Valley Progressive Network Calendar')
   end
 end
